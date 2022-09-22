e45fc52853c1000705c04ab73be5e821c8ac7a23
Dan Seminara <japaneseforgaming@gmail.com>
2022-09-21 22:21:30 -0400

    You can now click through pics

--- frontend/src/components/InfoCard.js
+++ frontend/src/components/InfoCard.js
@@ -2,10 +2,20 @@ import React from "react";
 
 function InfoCard({data}) {
 
+    function handleClick(e){
+        let xPos = e.pageX - e.target.offsetLeft
+//        let yPos = e.pageY - e.target.offsetTop
+//        let picSize = [e.target.clientWidth, e.target.clientHeight]
+        let halfwayPoint = e.target.clientWidth / 2
+//        console.log(xPos, yPos)
+        console.log(xPos > halfwayPoint) //if false, clicked on the left half, if true, clicked on the right half
+    
+    }
+
     return(
              <div className="userData">
                      <h3 className="centered">{data.user.UserName}</h3>
-                     <img className="avatarPic" src={data.avatar} /> 
+                     <img className="avatarPic" src={data.avatar} onClick={handleClick}/> 
                      <p className="centered">
                      Name: {data.user.ActualName} <br />
                      Pronouns: {data.user.Pronouns} <br />
@@ -13,6 +23,7 @@ function InfoCard({data}) {
                      <br />
                      {data.user.Description}
                      </p>
+                     <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
                  </div>
     )
 }
