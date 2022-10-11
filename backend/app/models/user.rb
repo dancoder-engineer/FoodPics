8f4824292a2f15e5ae100738840ad52fa80dfd04
Dan Seminara <japaneseforgaming@gmail.com>
2022-10-11 18:09:30 -0400

    Started comments

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
     has_secure_password
 
     has_many :posts
+    has_many :comments
     has_one_attached :avatar
 
     validates :UserName, format: { with: /\A[a-zA-Z0-9]+\Z/ }
+    validates :UserName, uniqueness: true
     validates :UserName, :ActualName, :Description, :Pronouns, :Website, :avatar, presence: true
 end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_01_191227) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_11_211747) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_01_191227) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "user_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "followings", force: :cascade do |t|
