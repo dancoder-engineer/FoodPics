b8c6821c5ee698ccfe6687832a1b5db200dae767
Dan Seminara <japaneseforgaming@gmail.com>
2022-10-14 15:43:30 -0400

    On this site, you can delete your past misdeeds

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
     has_secure_password
 
-    has_many :posts
-    has_many :comments
-    has_one_attached :avatar
-
+    has_many :posts, dependent: :destroy
+    has_many :comments, dependent: :destroy
+    has_one_attached :avatar, dependent: :destroy
+    
     validates :UserName, format: { with: /\A[a-zA-Z0-9]+\Z/ }
     validates :UserName, uniqueness: true
     validates :UserName, :ActualName, :Description, :Pronouns, :Website, :avatar, presence: true
