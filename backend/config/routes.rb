0771ad9e28d32af9dedf4c27cbf55fdabe6bcb20
Dan Seminara <japaneseforgaming@gmail.com>
2022-10-22 11:31:29 -0400

    Notifications started

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :notifications
   resources :messages
   resources :posttags
   resources :tags
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_19_215328) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_22_153025) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -58,6 +58,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_19_215328) do
     t.integer "sender"
     t.integer "recipient"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.string "content"
+    t.string "read"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
