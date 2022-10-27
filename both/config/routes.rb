7d2f38aa7ac46ef31f37912284b1a39bd0548550
Dan Seminara <japaneseforgaming@gmail.com>
2022-10-27 14:05:24 -0400

    Testing

--- both/config/routes.rb
+++ both/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api do
+ # namespace :api do
     resources :notifications
     resources :messages
     resources :posttags
@@ -32,7 +32,7 @@ Rails.application.routes.draw do
     get '/messagethreadback/:id', to: 'messages#messagethread'
     get '/commentnotifications/:id', to: 'notifications#commentnotifications'
     get '/readall/', to: 'notifications#readall'
-  end
+ # end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
